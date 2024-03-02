85a8e12a9d3138156f69e605e4e269666dade8b2
Zayd Alzein <zaydalzein@gmail.com>
2024-03-02 02:23:16 -0600

    ...

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ class Trader:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
+                    #orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
                 if int(best_bid) > acceptable_price:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                    #orders.append(Order(product, best_bid, -best_bid_amount))
             
             result[product] = orders
     
 		    # String value holding Trader state data required. 
 				# It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
+        traderData = state.traderData if state.traderData else "TraderData"
         
 				# Sample conversion request. Check more details below. 
         conversions = 1
